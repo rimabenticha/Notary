e143e4116eab02113c5a6eddd9b4aa8b8b97a869
FIRAS_HEDFI\firas <firashedfi4@gmail.com>
2025-12-11 22:59:37 +0100

    linked firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:noteary/core/utils/app_router.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Noteary());
 }
 
